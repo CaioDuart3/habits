bf1e98b3ed84f8f8b11ffba098ecd2e3122181c1
caioduart3 <caioduarte330@gmail.com>
2026-03-28 20:51:46 -0300

    feat: adiciona rotas de tarefas

---
+++ backend/src/routes/TaskRoutes.ts
@@ -0,0 +1,13 @@
+import {Router} from "express"
+import { TaskController } from "../controllers/TaskController.js"
+import { TaskService } from "../services/TaskService.js"
+import { TaskRepository } from "../repositories/TaskRepository.js"
+
+const router = Router()
+
+const repository = new TaskRepository()
+const service = new TaskService(repository)
+const controller = new TaskController(service)
+
+router.get("/tasks", controller.index.bind(controller))
+router.post("/tasks", controller.create.bind(controller))
