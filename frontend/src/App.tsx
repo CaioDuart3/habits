1a675ad9e7b2b70a1dd7dc61577c31e3b593f61d
caioduart3 <caioduarte330@gmail.com>
2026-03-30 01:05:41 -0300

    feat: adiciona frontend com conexão via axios

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -1,119 +1,78 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from './assets/vite.svg'
-import heroImg from './assets/hero.png'
-import './App.css'
+import { Table, TableBody, TableHead, TableHeader, TableRow, TableCell} from "./components/ui/table"
+import { Button } from "./components/ui/button"
+import { Input  } from "./components/ui/input" // se o arquivo exporta mais de 1 coisa, é com {}
+import api from "./services/api" // se o arquivo exporta 1 coisa, é sem {}
+import { useEffect, useState } from "react"
+import type { Task } from "./types/Task" //importa a tipagem de Task para usar no frontend, garantindo que os dados estejam corretos e evitando erros de tipo.
+
 
 function App() {
-  const [count, setCount] = useState(0)
 
+  // solicitar request
+  const [tasks, setTasks] = useState<Task[]>([])
+
+  useEffect(() => {
+    api.get("/tasks")
+      .then(response => {
+        setTasks(response.data)
+      })
+      .catch(error => {
+        console.error("Erro ao buscar tarefas:", error)
+      })
+  }, [])
+
+  // criar função para criar tarefa
+  const[title, setTitle] = useState("")
+  const[description, setDescription] = useState("")
+  const[status, setStatus] = useState("")
+
+  const handleCriarTarefa = () => {
+    api.post("/tasks", {
+      title,
+      description,
+      status
+    })
+    .then(response => {
+      setTasks([...tasks, response.data]) //atualiza a lista de tarefas com a nova tarefa criada
+      setTitle("") //limpa os campos de input
+      setDescription("")
+      setStatus("")
+    })
+    .catch(error => {
+      console.error("Erro ao criar tarefa:", error)
+    })
+  }
+  // renderizar a interface
   return (
     <>
-      <section id="center">
-        <div className="hero">
-          <img src={heroImg} className="base" width="170" height="179" alt="" />
-          <img src={reactLogo} className="framework" alt="React logo" />
-          <img src={viteLogo} className="vite" alt="Vite logo" />
-        </div>
-        <div>
-          <h1>Get started</h1>
-          <p>
-            Edit <code>src/App.tsx</code> and save to test <code>HMR</code>
-          </p>
-        </div>
-        <button
-          className="counter"
-          onClick={() => setCount((count) => count + 1)}
-        >
-          Count is {count}
-        </button>
-      </section>
+      <Input placeholder="Titulo" value={title} onChange={(e) => setTitle(e.target.value)} />
+      <Input placeholder="Descrição" value={description} onChange={(e) => setDescription(e.target.value)} />
+      <Input placeholder="Status" value={status} onChange={(e) => setStatus(e.target.value)} />
+    
+      <Button onClick={handleCriarTarefa}>Criar tarefa</Button>
 
-      <div className="ticks"></div>
+      <Table>
+        <TableHeader>
+          <TableRow>
+            <TableHead>Titulo</TableHead>
+            <TableHead>Descrição</TableHead>
+            <TableHead>Status</TableHead>
+            <TableHead>Última atualização</TableHead>
+          </TableRow>
+        </TableHeader>
 
-      <section id="next-steps">
-        <div id="docs">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#documentation-icon"></use>
-          </svg>
-          <h2>Documentation</h2>
-          <p>Your questions, answered</p>
-          <ul>
-            <li>
-              <a href="https://vite.dev/" target="_blank">
-                <img className="logo" src={viteLogo} alt="" />
-                Explore Vite
-              </a>
-            </li>
-            <li>
-              <a href="https://react.dev/" target="_blank">
-                <img className="button-icon" src={reactLogo} alt="" />
-                Learn more
-              </a>
-            </li>
-          </ul>
-        </div>
-        <div id="social">
-          <svg className="icon" role="presentation" aria-hidden="true">
-            <use href="/icons.svg#social-icon"></use>
-          </svg>
-          <h2>Connect with us</h2>
-          <p>Join the Vite community</p>
-          <ul>
-            <li>
-              <a href="https://github.com/vitejs/vite" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#github-icon"></use>
-                </svg>
-                GitHub
-              </a>
-            </li>
-            <li>
-              <a href="https://chat.vite.dev/" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#discord-icon"></use>
-                </svg>
-                Discord
-              </a>
-            </li>
-            <li>
-              <a href="https://x.com/vite_js" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#x-icon"></use>
-                </svg>
-                X.com
-              </a>
-            </li>
-            <li>
-              <a href="https://bsky.app/profile/vite.dev" target="_blank">
-                <svg
-                  className="button-icon"
-                  role="presentation"
-                  aria-hidden="true"
-                >
-                  <use href="/icons.svg#bluesky-icon"></use>
-                </svg>
-                Bluesky
-              </a>
-            </li>
-          </ul>
-        </div>
-      </section>
+        <TableBody>
+          {tasks.map((task: any) => (
+            <TableRow key={task.id}>
+              <TableCell>{task.title}</TableCell>
+              <TableCell>{task.description}</TableCell>
+              <TableCell>{task.status}</TableCell>
+              <TableCell>{new Date(task.updated_at).toLocaleString()}</TableCell>
+            </TableRow>
+          ))}
+        </TableBody>
 
-      <div className="ticks"></div>
-      <section id="spacer"></section>
+      </Table>
     </>
   )
 }
